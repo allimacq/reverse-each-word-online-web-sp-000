b7b3c034e2b3cdbf577a0d8f2ed3aab214f64469
Allison MacQueen <allimacq@gmail.com>
2020-08-29 17:38:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(word)
   string = Array.new()
   array = word.split( )
   array.collect do |x| 
-    string = x.reverse
+    string << x.reverse
   end
   p string.join(" ")
 end
